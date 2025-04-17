c8cd9d0566fc999a6929892db107b835c1624f7e
vishnu1002 <vishnuramguru@gmail.com>
2025-04-17 08:20:59 +0000

    added basic services

---
+++ homepage/server.mjs
@@ -0,0 +1,18 @@
+import express from 'express';
+import os from 'os';
+import path from 'path';
+
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(process.cwd()));
+
+app.get('/', (req, res) => {
+  const hostname = os.hostname();
+  res.render('index', { hostname });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
